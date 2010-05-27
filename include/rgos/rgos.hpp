2b9fb8f3269e7e5559f1a1dd1dba3aa38640d0d4
Chris Pickel <sfiera@gmail.com>
2010-05-26 23:35:10 -0400

    Add rgos/rgos.hpp master header.

---
+++ include/rgos/rgos.hpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2009 Chris Pickel <sfiera@gmail.com>
+//
+// This file is part of librgos, a free software project.  You can redistribute it and/or modify it
+// under the terms of the MIT License.
+
+#ifndef RGOS_RGOS_HPP_
+#define RGOS_RGOS_HPP_
+
+#include "rgos/Json.hpp"
+#include "rgos/JsonVisitor.hpp"
+#include "rgos/Serialize.hpp"
+
+#endif  // RGOS_RGOS_HPP_
